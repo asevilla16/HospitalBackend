6766fe27310937c64402879e04c1edb775747cba
asevilla16 <angelsevilla1997@gmail.com>
2023-01-01 23:01:22 -0600

    Seccion 12 - Google

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,15 +2,16 @@ const Usuario = require('../models/usuario');
 const bcrypt = require("bcryptjs");
 const { response } = require("express");
 const { generateJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
-const login = async(req, res = response) => {
+const login = async (req, res = response) => {
     try {
 
-        const {email, password} = req.body;
+        const { email, password } = req.body;
 
-        const existingUser = await Usuario.findOne({email});
+        const existingUser = await Usuario.findOne({ email });
 
-        if(!existingUser){
+        if (!existingUser) {
             return res.status(404).json({
                 ok: false,
                 msg: 'No existe ningun usuario con el correo o password especificado'
@@ -18,7 +19,7 @@ const login = async(req, res = response) => {
         }
 
         const validPassword = bcrypt.compareSync(password, existingUser.password);
-        if(!validPassword){
+        if (!validPassword) {
             return res.status(400).json({
                 ok: false,
                 msg: 'Contraseña no valida'
@@ -42,6 +43,50 @@ const login = async(req, res = response) => {
     }
 }
 
+const googleSignIn = async (req, res = response) => {
+
+    try {
+        const {email, name, picture} = await googleVerify(req.body.token);
+
+        const usuarioDb = await Usuario.findOne({email});
+        let usuario;
+
+        if(!usuarioDb){
+            usuario = new Usuario({
+                nombre: name,
+                email,
+                img: picture,
+                password: '@@@',
+                google: true
+            });
+        } else {
+            usuario = usuarioDb;
+            usuario.google = true;
+        }
+
+        await usuario.save();
+
+        const token = await generateJWT(usuario.id)
+
+        res.json({
+            ok: true,
+            email,
+            name,
+            picture,
+            token
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            ok: false,
+            msg: 'Token no valido'
+        })
+    }
+
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
